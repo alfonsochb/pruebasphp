a1de20bef9d63a4aa240d2a3a48eebc41b8af473
ALFONSO CHAVEZ <achavezb@siesa.com>
2023-05-11 22:21:17 -0500

    Tercer commit

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ use Achb\AchbLibrary\Class\AchbResources AS Resource;
  * @link https://blog.pleets.org/article/crear-librer%C3%ADa-en-php-instalable-con-composer
  * @link https://culttt.com/2014/05/07/create-psr-4-php-package/
  * @link https://styde.net/creacion-de-una-nueva-libreria-para-laravel-con-composer-y-phpunit/
+ * @link https://www.imaginacolombia.com/articulos/7-librerias-de-php-que-todo-desarrollador-web-deberia-conocer
+ * s
  */
 class Pruebas
 {
